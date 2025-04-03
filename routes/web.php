a4732d7f15e60e9450f1e34e7815253da4cb99fa
Shadi Abdelslam <shadodev@gmail.com>
2025-04-03 05:13:26 +0200

    Add authentication middleware to Post resource routes

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::resource('posts', PostController::class);
+Route::resource('posts', PostController::class)->middleware(['auth', 'verified']);
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
